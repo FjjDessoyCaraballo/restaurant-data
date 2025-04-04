8db42b0962ceadb1f83198a1911ea28dd28afa25
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2025-04-04 16:41:06 +0300

    nofix: nothing to fix

--- task1/sql/queries/query1.sql
+++ task1/sql/queries/query1.sql
@@ -1,8 +1,6 @@
 -- -- [Task] Query 1
 
--- SELECT COUNT(*) AS num_users_registered
--- FROM Users
--- WHERE User_country = 'FIN'
---   AND User_registration_timestamp_utc >= datetime('now', '30 days')
---   -- AND User_registration_timestamp_utc >= NOW() - INTERVAL '30 days';
+SELECT COUNT(*) FROM Users WHERE User_country = 'FIN' 
+  -- AND User_registration_timestamp_utc >= datetime('now', '-30 days')
+  -- AND User_registration_timestamp_utc >= NOW() - INTERVAL '30 days';
 
